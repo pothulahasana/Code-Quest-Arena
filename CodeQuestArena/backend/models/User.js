4eccff6c22da547e1115e831693334dd69b9db7b
Madhavi0967 <nakkamadhavi009@gmail.com>
2026-04-08 06:33:20 +0000

    Add CodeQuestArena folder

---
+++ CodeQuestArena/backend/models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+
+  // 🪙 Coins wallet
+  coins: {
+    type: Number,
+    default: 0
+  }
+});
+
+module.exports = mongoose.model("User", userSchema);
